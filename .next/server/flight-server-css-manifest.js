a4991d6b72503e63931c6b222be8905c111de204
SanC005 <82254558+ReiZero1349@users.noreply.github.com>
2023-04-20 00:25:05 +0530

    fix

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,23 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\layout.js": [
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\Components\\Styles\\NavBar.css",
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\globals.css"
+    ],
+    "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\page.js": [
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\Components\\Styles\\NavBar.css",
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\node_modules\\bootstrap\\dist\\css\\bootstrap.min.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\page": [
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\Components\\Styles\\NavBar.css",
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\node_modules\\bootstrap\\dist\\css\\bootstrap.min.css",
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\globals.css"
+    ],
+    "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\about\\page": [
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\Components\\Styles\\NavBar.css",
+      "C:\\Users\\San22\\OneDrive\\Desktop\\Journey\\NextRouteWeather\\on-the-route-weather\\src\\app\\globals.css"
+    ]
+  }
+}
---
+++ .next/static/webpack/app/layout.57ebccad4bdaf6a0.hot-update.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/layout",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CSan22%5COneDrive%5CDesktop%5CJourney%5CNextRouteWeather%5Con-the-route-weather%5Csrc%5Capp%5Cglobals.css&server=false!":
+/*!***************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CSan22%5COneDrive%5CDesktop%5CJourney%5CNextRouteWeather%5Con-the-route-weather%5Csrc%5Capp%5Cglobals.css&server=false! ***!
+  \***************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/app/globals.css */ \"(app-client)/./src/app/globals.css\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPUMlM0ElNUNVc2VycyU1Q1NhbjIyJTVDT25lRHJpdmUlNUNEZXNrdG9wJTVDSm91cm5leSU1Q05leHRSb3V0ZVdlYXRoZXIlNUNvbi10aGUtcm91dGUtd2VhdGhlciU1Q3NyYyU1Q2FwcCU1Q2dsb2JhbHMuY3NzJnNlcnZlcj1mYWxzZSEuanMiLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz85ZDU5Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiQzpcXFxcVXNlcnNcXFxcU2FuMjJcXFxcT25lRHJpdmVcXFxcRGVza3RvcFxcXFxKb3VybmV5XFxcXE5leHRSb3V0ZVdlYXRoZXJcXFxcb24tdGhlLXJvdXRlLXdlYXRoZXJcXFxcc3JjXFxcXGFwcFxcXFxnbG9iYWxzLmNzc1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CSan22%5COneDrive%5CDesktop%5CJourney%5CNextRouteWeather%5Con-the-route-weather%5Csrc%5Capp%5Cglobals.css&server=false!\n"));
+
+/***/ }),
+
+/***/ "(app-client)/./src/app/globals.css":
+/*!*****************************!*\
+  !*** ./src/app/globals.css ***!
+  \*****************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"c71af3bbb009\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vc3JjL2FwcC9nbG9iYWxzLmNzcy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL2FwcC9nbG9iYWxzLmNzcz8wYWE4Il0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IFwiYzcxYWYzYmJiMDA5XCJcbmlmIChtb2R1bGUuaG90KSB7IG1vZHVsZS5ob3QuYWNjZXB0KCkgfVxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./src/app/globals.css\n"));
+
+/***/ })
+
+});
